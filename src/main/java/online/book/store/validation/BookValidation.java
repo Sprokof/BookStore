fd775e35f720739fda790bd259cf3666a0821169
Sergei Prokofev <hergod@outlook.com>
2022-07-17 22:20:14 +0400

    append addBook.html and book validation

--- src/main/java/online/book/store/validation/BookValidation.java
+++ src/main/java/online/book/store/validation/BookValidation.java
@@ -19,7 +19,24 @@ public class BookValidation implements Validator {
 
             BookDto bookDto = (BookDto) o;
 
-            if(bookDto.getBookImage() == null)
+            if(bookDto.getBookImage() == null){
+                errors.rejectValue("bookImage", "Insert.book.image");
+            }
+
+            if(!imageFile(bookDto.getBookImage().getName())){
+                errors.rejectValue("bookImage", "Not.image.format");
+            }
     }
 
+
+    private boolean imageFile(String name){
+        String currentFormat = name.substring(name.indexOf("."));
+        String[] imagesFormat = {".png", ".jpg", ".bmp", ".ico", ".jpeg", ".pnm"};
+        for(String format : imagesFormat){
+            if(currentFormat.equals(format)){
+                return true;
+            }
+        }
+    return false;
+    }
 }
